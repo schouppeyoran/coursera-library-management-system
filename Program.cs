1973a920b86213a791c5bb832bb4d8ca9779af49
Yoran Desktop <schouppey@gmail.com>
2024-12-19 01:11:50 +0100

    add: borrowing/returning feature

--- Program.cs
+++ Program.cs
@@ -3,11 +3,13 @@
     static void Main()
     {
         List<string> books = new List<string>();
+        List<string> borrowedBooks = new List<string>();
         const int maxBooks = 5;
+        const int maxBorrowedBooks = 3;
 
         while (true)
         {
-            Console.WriteLine("Would you like to add, remove, list, or exit? (add/remove/list/search/exit)");
+            Console.WriteLine("Would you like to add, remove, list, search, borrow, return, or exit? (add/remove/list/search/borrow/return/exit)");
             string action = Console.ReadLine().ToLower();
 
             if (action == "add")
@@ -71,13 +73,56 @@
                     Console.WriteLine("Book not found.");
                 }
             }
+            else if (action == "borrow")
+            {
+                if (borrowedBooks.Count >= maxBorrowedBooks)
+                {
+                    Console.WriteLine("You have already borrowed the maximum number of books.");
+                }
+                else
+                {
+                    Console.WriteLine("Enter the title of the book to borrow:");
+                    string borrowBook = Console.ReadLine();
+                    if (books.Contains(borrowBook))
+                    {
+                        borrowedBooks.Add(borrowBook);
+                        books.Remove(borrowBook);
+                        Console.WriteLine("Book borrowed.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Book not found.");
+                    }
+                }
+            }
+            else if (action == "return")
+            {
+                if (borrowedBooks.Count == 0)
+                {
+                    Console.WriteLine("You have not borrowed any books.");
+                }
+                else
+                {
+                    Console.WriteLine("Enter the title of the book to return:");
+                    string returnBook = Console.ReadLine();
+                    if (borrowedBooks.Remove(returnBook))
+                    {
+                        books.Add(returnBook);
+                        Console.WriteLine("Book returned.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Book not found.");
+                    }
+                }
+            }
             else if (action == "exit")
             {
                 break;
             }
             else
             {
-                Console.WriteLine("Invalid action. Please type 'add', 'remove', 'list', or 'exit'.");
+                Console.WriteLine("Invalid action. Please type 'add', 'remove', 'list', 'search', 'borrow', 'return', or 'exit'.");
             }
         }
     }
